9c2a207507bb8de91e629c61a4e4927d7d4a0b63
Meysam Azad <MeysamAzad81@gmail.com>
2022-09-30 22:55:43 +0700

    feat: customize port and token ✨

--- scripts/jupyter/entrypoint.sh
+++ scripts/jupyter/entrypoint.sh
@@ -1,9 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 if [[ -d ${PIP_EXTRAS} ]]; then
     echo "Installing from ${PIP_EXTRAS} ..."
     ls ${PIP_EXTRAS}
     find ${PIP_EXTRAS} -name 'requirements*.txt' -exec pip install -r {} \;
 fi
+
+TMP=$(mktemp)
+sed "s/PORT/${PORT:-8888}/g; s/TOKEN/${TOKEN:-lware}/g" /jupyter_notebook_config.py > ${TMP}
+cp ${TMP} /jupyter_notebook_config.py
 
 exec jupyter notebook --config /jupyter_notebook_config.py
--- scripts/jupyter/jupyter_notebook_config.py
+++ scripts/jupyter/jupyter_notebook_config.py
@@ -2,5 +2,5 @@ c.NotebookApp.allow_remote_access = True
 c.NotebookApp.ip = "0.0.0.0"
 c.NotebookApp.open_browser = False
 c.NotebookApp.password_required = False
-c.NotebookApp.port = 8888
-c.NotebookApp.token = "lware"
+c.NotebookApp.port = PORT
+c.NotebookApp.token = "TOKEN"
